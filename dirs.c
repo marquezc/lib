1a92bef9696612032ff432eaccdeed0c979d9ad5
marquezc <chrismarquez329@gmail.com>
2014-02-23 12:06:59 -0600

    modified dirs.c

--- dirs.c
+++ dirs.c
@@ -4,12 +4,12 @@
 #include <unistd.h>
 
 #define MAXFILENAME			255
-char		*filename;
+char  *filename;
 
 int
 main(int argc, char *argv[])
 {
-  DIR						*dp;
+  DIR   *dp;
   struct dirent *dirent;
 
   if (argc > 2)
@@ -18,14 +18,14 @@ main(int argc, char *argv[])
       exit(1);
     }
 
-	filename = (argc == 1) ? getcwd(filename, MAXFILENAME) : argv[1];
-	if ((dp = opendir(filename)) == NULL)
+  filename = (argc == 1) ? getcwd(filename, MAXFILENAME) : argv[1];
+  if ((dp = opendir(filename)) == NULL)
     {
       printf("Error: couldn't open %s\n", argv[1]);
       exit(1);
     }
 
-	while ((dirent = readdir(dp)) != NULL)
+  while ((dirent = readdir(dp)) != NULL)
     if (dirent->d_name[0] != '.')
       printf("%s\n", dirent->d_name);
 }
