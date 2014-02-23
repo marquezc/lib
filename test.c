f873028e234a740ec926b008e50b70aaad873b8f
marquezc <chrismarquez329@gmail.com>
2014-02-22 21:15:08 -0600

    modified test.c

--- test.c
+++ test.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main (void)
+static char *progname = "test";
+static char *filename;
+
+int    cext(char *);
+
+#define MAXLINE 80
+#define USGERR printf("Usage: %s filename.c\n", progname);
+
+struct dtype
 {
-  printf("git test file\n");
+  char  *tag;
+  int    cnt;
+} dtypes[] = {
+  {" char ", 0},
+  {" short ", 0},
+  {" int ", 0},
+  {" long ", 0},
+  {" double ", 0},
+  {" float ", 0}
+};
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2 || !cext(argv[1])) {
+      USGERR;
+      exit(1);
+  }
+
+  filename = argv[1];
+
+  FILE  *fp = fopen(filename, "r");
+	char buf[MAXLINE];
+	
+  while (fgets(buf, MAXLINE, fp))
+    {
+			int i;
+			for (i = 0; i < 6; i++) {
+				struct dtype *dtype = &dtypes[i];
+				if (strstr(buf, dtype->tag))
+					fputs(buf, stdout);
+			}
+    }
 }
+
+int
+cext(char *name)
+{
+  int    x = strlen(name) - 2;
+	return strstr(name + x, ".c") ? 1 : 0;
+}
+
